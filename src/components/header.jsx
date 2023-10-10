8a30feb65bb304f59ee6cb268c1717f7311f6f96
mehdias63 <137681653+mehdias63@users.noreply.github.com>
2023-10-10 14:36:26 +0330

    init project

---
+++ src/components/header.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+export default function Header(){
+    return(
+        <section className="header">
+            <div className="head">
+            <div>
+            <h2 className="title">MEETME</h2>
+            </div>
+            <div className="menu">
+            <ul>
+            <li><a href="#home">Home</a></li>
+            <li><a href="#about">ABOUT</a></li>
+            <li><a href="#services">SERVICES</a></li>
+            <li><a href="#pages">PAGES</a></li>
+            <li><a href="#blog">Blog</a></li>
+            <li><a href="#contact">CONTACT</a></li>
+        </ul>
+        </div>
+        </div>
+        <div className="info">
+            
+        </div>
+            </section>
+    )
+}
