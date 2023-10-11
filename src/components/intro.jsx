db096bcc805b4d99b0e2e09dfe262c108bb7676d
mehdias63 <137681653+mehdias63@users.noreply.github.com>
2023-10-11 13:59:26 +0330

    update intro

--- src/components/intro.jsx
+++ src/components/intro.jsx
@@ -6,6 +6,32 @@ export default function Intro(){
             <button>My Experiences</button>
             <button>My Education</button>
             </div>
+            <div className="count">
+            <div >
+                <h3>March 2017 to present</h3>
+                <h3>March 2017 to present</h3>
+                <h3>March 2017 to present</h3>
+            </div>
+            <div>
+                <div className="container">
+                <span></span>
+                <span></span>
+                <span></span>
+                </div>
+                
+            </div>
+            <div>
+                <h1>COLORLIB</h1>
+                <h4>Senior Web Developer</h4>
+                <h4>Santa monica, Los angeles</h4>
+                <h1>COLORLIB</h1>
+                <h4>Senior Web Developer</h4>
+                <h4>Santa monica, Los angeles</h4>
+                <h1>COLORLIB</h1>
+                <h4>Senior Web Developer</h4>
+                <h4>Santa monica, Los angeles</h4>
+            </div>
+            </div>
         </div>
     )
     
