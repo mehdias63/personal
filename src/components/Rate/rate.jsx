d941306b99e22d80c3f98804de37778554ec860d
mehdias63 <137681653+mehdias63@users.noreply.github.com>
2023-11-01 12:45:39 +0330

    final update

--- src/components/Rate/rate.jsx
+++ src/components/Rate/rate.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react'
 import { FaStar } from "react-icons/fa6";
 
-const possibel = [1,2,3,4,5]
+const possible = [1,2,3,4,5]
 
 export default function Rate() {
     const [selected , setSelected]=useState(null);
@@ -14,9 +14,9 @@ export default function Rate() {
         </div>
         <div className='rate-bottom'>
             <div className='star flex justify-center'>
-                {possibel.map(rate => 
+                {possible.map(rate => 
                     <i key={rate} 
-                    className={((rate <= selected) ? 'in-rate' : '') + 
+                    className={((rate <= selected && !hoverd) ? 'in-rate' : '') + 
                 ((rate <= hoverd) ? 'in-hover' : '')}
                     onClick={() => setSelected(rate)}
                     onMouseEnter={() => setHoverd(rate)}
