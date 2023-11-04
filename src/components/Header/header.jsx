ca9242b64301f16398d699381b97338eb9cebb5a
mehdias63 <137681653+mehdias63@users.noreply.github.com>
2023-11-04 15:00:17 +0330

    update header 2

--- src/components/Header/header.jsx
+++ src/components/Header/header.jsx
@@ -9,41 +9,81 @@ import { SlSocialTwitter } from "react-icons/sl";
 import { SlSocialLinkedin } from "react-icons/sl";
 import { GiHamburgerMenu } from "react-icons/gi";
 import headata from "./headata";
-export default function Header(){
-    return(
-        <section className="header" id="home" >
-            <div className="head " >
-            <div>
-            <h2 className="title">MEETME</h2>
-            </div>
-             <div className="menu ">
-             <label htmlFor="hamburger-menu"><GiHamburgerMenu /></label>
-            <input type="checkbox" id="hamburger-menu" />
-            <nav>
+export default function Header() {
+  return (
+    <section className="header" id="home">
+      <div className="head ">
+        <div>
+          <h2 className="title">MEETME</h2>
+        </div>
+        <div className="menu ">
+          <label htmlFor="hamburger-menu">
+            <GiHamburgerMenu />
+          </label>
+          <input type="checkbox" id="hamburger-menu" />
+          <nav>
             <ul>
-            {headata.map(card => < HeadCard key={card.id} titel={card.titel} id={card.id} /> )}
-        </ul>
-        </nav>
+              {headata.map((card) => (
+                <HeadCard key={card.id} titel={card.titel} id={card.id} />
+              ))}
+            </ul>
+          </nav>
         </div>
-        </div>
-        <div className="info">
+      </div>
+      <div className="info">
         <img src="../images/5934.JPG" />
         <div>
-            <p>HELLO EVERYBDY, I AM</p>
-            <h1>MEHDI ASADI</h1>
-            <h4>JUNIOR UI/UX DEVELOPER</h4>
-            <p className="second">You will begin to realise why this exercise is called the Dickens Pattern (with reference to the ghost showing Scrooge some different futures)</p>
-            <span><i><HiOutlineCalendarDays /></i>23st December, 1984</span>
-            <span><i><FiPhone /></i>44 (012) 6954 783</span>
-            <span><i><AiOutlineMail /></i>businessplan@donald</span>
-            <span><i><AiOutlineHome /></i>Santa monica bullevard</span>
-            <div>
-                <span><i><LuFacebook /></i></span>
-                <span><i><SlSocialTwitter /></i></span>
-                <span><i><SlSocialLinkedin /></i></span>
-            </div>
+          <p>HELLO EVERYBDY, I AM</p>
+          <h1>MEHDI ASADI</h1>
+          <h4>JUNIOR UI/UX DEVELOPER</h4>
+          <p className="second">
+            You will begin to realise why this exercise is called the Dickens
+            Pattern (with reference to the ghost showing Scrooge some different
+            futures)
+          </p>
+          <span>
+            <i>
+              <HiOutlineCalendarDays />
+            </i>
+            23st December, 1984
+          </span>
+          <span>
+            <i>
+              <FiPhone />
+            </i>
+            44 (012) 6954 783
+          </span>
+          <span>
+            <i>
+              <AiOutlineMail />
+            </i>
+            businessplan@donald
+          </span>
+          <span>
+            <i>
+              <AiOutlineHome />
+            </i>
+            Santa monica bullevard
+          </span>
+          <div>
+            <span>
+              <i>
+                <LuFacebook />
+              </i>
+            </span>
+            <span>
+              <i>
+                <SlSocialTwitter />
+              </i>
+            </span>
+            <span>
+              <i>
+                <SlSocialLinkedin />
+              </i>
+            </span>
+          </div>
         </div>
-        </div>
-            </section>
-    )
+      </div>
+    </section>
+  );
 }
--- src/components/Intro/intro.jsx
+++ src/components/Intro/intro.jsx
@@ -1,16 +1,23 @@
-import React from "react"
-import Button from "../Button"
-import Intcard from "./Intcard"
-import intdata from "./intdata"
-export default function Intro(){
-    return(
-        <div className="intro" id="blog">
-            <div className="btn">
-            <Button size="lg">My Experiences</Button>
-            <Button size="lg">My Education</Button>
-            </div>
-            {intdata.map(card => <Intcard key={card.id} titel={card.titel} description={card.description} text={card.text} textdat={card.textdat} /> )}
-        </div>
-    )
-    
+import React from "react";
+import Button from "../Button";
+import Intcard from "./Intcard";
+import intdata from "./intdata";
+export default function Intro() {
+  return (
+    <div className="intro" id="blog">
+      <div className="btn">
+        <Button size="lg">My Experiences</Button>
+        <Button size="lg">My Education</Button>
+      </div>
+      {intdata.map((card) => (
+        <Intcard
+          key={card.id}
+          titel={card.titel}
+          description={card.description}
+          text={card.text}
+          textdat={card.textdat}
+        />
+      ))}
+    </div>
+  );
 }
